9e3b6fe61bb5bdd14a22810b8b208d0c1f906925
zeru0429 <mihiretutigistu@gmail.com>
2023-08-25 20:08:40 +0300

    database and express config

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import {connection} from './config/db.js'
+
+// config .env
+dotenv.config();
+const port = process.env.SERVER_PORT;
+const host = process.env.SERVER_HOST;
+
+
+
+const server = express();
+
+server.get('/', (req, res) => { 
+    res.send(`<h1>working ... </h1>`);
+});
+
+
+
+
+server.listen(port, host, (error) => { 
+    if (error) console.log(error);
+    console.log(`http://${host}:${[port]}`);
+});
