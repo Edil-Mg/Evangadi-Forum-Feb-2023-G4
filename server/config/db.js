3990de06ea15c250d188151cd61205eba92e0879
zeru0429 <mihiretutigistu@gmail.com>
2023-08-30 15:43:05 +0300

    remove comments from db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,21 +1,17 @@
 import mysql from 'mysql2';
-// const port = process.env.DB_PORT;
-// const host = process.env.DB_HOST;
-// const database = process.env.DB_NAME;
-// const user = process.env.DB_USER;
-// const password = process.env.DB_PASSWORD;
-// console.log(port);
+import dotenv from 'dotenv';
+dotenv.config();
 
-const port = 3333;
-const host = 'localhost';
-const database = 'EvangadiForem2';
-const user = 'root';
-const password = '1234';
-// console.log(port);
+const port = process.env.DB_PORT;
+const host = process.env.DB_HOST;
+const database = process.env.DB_NAME;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+console.log(port);
 
 export const connection = mysql.createConnection({
 	host,
-	// port,
+	port,
 	database,
 	user,
 	password,
